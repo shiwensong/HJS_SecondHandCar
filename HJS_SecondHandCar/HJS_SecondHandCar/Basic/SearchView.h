e44e7538d5e63f21fb62ef75965a92d35bf4b67e
shiwensong <18996601419@189.cn>
2015-10-19 21:11:12 +0800

    End of the first day, framework has been completed, but the home page of the search box didn't complete!

---
+++ HJS_SecondHandCar/HJS_SecondHandCar/Basic/SearchView.h
@@ -0,0 +1,15 @@
+//
+//  SearchView.h
+//  TensWeiBo_demo
+//
+//  Created by tens on 15-10-5.
+//  Copyright (c) 2015年 tens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchView : UITextField
+
+
+
+@end
